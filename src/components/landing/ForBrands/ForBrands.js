0e224306e4badcb41ce724779ef841806dfc3d6b
MFMercado <ar.mflmercado@gmail.com>
2023-11-26 22:58:43 +0800

    for brands and collectors updates

--- src/components/landing/ForBrands/ForBrands.js
+++ src/components/landing/ForBrands/ForBrands.js
@@ -37,28 +37,28 @@ export default function ForBrands() {
   };
 
   return (
-    <Grid container direction={{ desktop: "row-reverse" }}>
-      <Grid item desktop={6} className={styles.brandsGridItem}>
-        <Stack
-          className={styles.brandsTitle}
-          sx={{
-            padding: {
-              mobile: "40px 24px",
-              tablet: "72px 100px",
-              desktop: "136px 100px 64px 72px",
-            },
-          }}
-        >
+    <Grid
+      container
+      columnSpacing="144px"
+      direction={{ desktop: "row-reverse" }}
+      sx={{
+        padding: { mobile: "40px 24px", tablet: "136px 100px" },
+        rowGap: { mobile: "80px" },
+      }}
+    >
+      <Grid
+        item
+        desktop={6}
+        className={styles.brandsGridItem}
+        sx={{ gap: { mobile: "80px", tablet: "64px" } }}
+      >
+        <Stack className={styles.brandsTitle}>
           <Typography sx={{ typography: { mobile: "h2-mobile", tablet: "h2" } }}>
             FOR BRANDS
           </Typography>
           {/* <Image src="/icons/asterisk.svg" alt="icon" width={80} height={80} /> */}
         </Stack>
-        <Stack
-          sx={{
-            padding: { mobile: "40px 24px", tablet: "0 100px 72px", desktop: "0 72px 72px 100px" },
-          }}
-        >
+        <Stack>
           <Stack className={styles.brandsImages}>
             <Box className={styles.brandsImagesGlow} sx={{ backgroundColor: "info.main" }} />
             <Box className={styles.brandsWindow1} sx={{ left: { mobile: "0", desktop: "-24px" } }}>
@@ -83,7 +83,13 @@ export default function ForBrands() {
                 <Image src="/icons/playlist_buttons.svg" alt="buttons" width={120} height={18} />
               </Window>
             </Box>
-            <Box className={styles.brandsWindow2} sx={{ left: { mobile: "0", desktop: "-8px" } }}>
+            <Box
+              className={styles.brandsWindow2}
+              sx={{
+                left: { desktop: "-8px" },
+                bottom: { mobile: "0", desktop: "52px" },
+              }}
+            >
               <Window title="CloneX #19799">
                 <Image src="/icons/check_circle.svg" alt="check" width={34} height={34} />
                 <Typography>
@@ -100,38 +106,24 @@ export default function ForBrands() {
           </Stack>
         </Stack>
       </Grid>
-      <Grid item desktop={6} className={styles.brandsGridItem}>
-        <Box
-          sx={{
-            padding: {
-              mobile: "40px 24px",
-              tablet: "0 100px 72px",
-              desktop: "136px 72px 64px 100px",
-            },
-          }}
-        >
-          <TextBox>
-            <Typography color="text.gray">
-              From Adidas to Nightclub E11even, businesses are “apeing in” to the NFT space. They
-              often purchase an NFT themselves (the Florida nightclub recently paid more then
-              $400,000 for a single Bored Ape NFT). IP-Fi enables your brand to collaborate with NFT
-              personas across multiple collections and blockchains. Start partnering with Bored
-              Apes, RTFKT Clones, Dead Fellas, and many other top NFT personas without having to buy
-              the underlying NFTs.
-            </Typography>
-          </TextBox>
-        </Box>
-        <Box
-          sx={{
-            padding: {
-              mobile: "40px 24px",
-              tablet: "0 100px 72px",
-              desktop: "64px 72px 136px 100px",
-            },
-          }}
-        >
-          <CustomList list={brandsBenefit} />
-        </Box>
+      <Grid
+        item
+        desktop={6}
+        className={styles.brandsGridItem}
+        sx={{ gap: { mobile: "80px", tablet: "128px" } }}
+      >
+        <TextBox>
+          <Typography color="text.gray">
+            From Adidas to Nightclub E11even, businesses are “apeing in” to the NFT space. They
+            often purchase an NFT themselves (the Florida nightclub recently paid more then $400,000
+            for a single Bored Ape NFT). IP-Fi enables your brand to collaborate with NFT personas
+            across multiple collections and blockchains. Start partnering with Bored Apes, RTFKT
+            Clones, Dead Fellas, and many other top NFT personas without having to buy the
+            underlying NFTs.
+          </Typography>
+        </TextBox>
+
+        <CustomList list={brandsBenefit} />
       </Grid>
     </Grid>
   );
