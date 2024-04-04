d389fe2413e3f7ecad6477d383da6fe999412f76
fayeMercado <ar.mflmercado@gmail.com>
2024-04-04 19:43:37 +0800

    circle component

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -20,3 +20,4 @@ export { default as WalletList } from "./WalletList";
 export * from "./Catalog";
 export { PasswordInput } from "./Input";
 export { BackgroundCircles } from "./Background";
+export { default as Circle } from "./Circle";
