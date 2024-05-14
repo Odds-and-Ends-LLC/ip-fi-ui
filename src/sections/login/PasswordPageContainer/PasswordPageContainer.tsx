5902d2b82b2281465849b3fcc77b4ddf3c44a0f5
fayeMercado <ar.mflmercado@gmail.com>
2024-05-14 23:47:05 +0800

    update password page container

--- src/sections/login/PasswordPageContainer/PasswordPageContainer.tsx
+++ src/sections/login/PasswordPageContainer/PasswordPageContainer.tsx
@@ -1,25 +1,32 @@
 // packages
 import { ReactNode } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 // styles
 import styles from "./PasswordPageContainer.module.css";
 
 // components
+import { Circle } from "@/components";
 
 export default function PasswordPageContainer({ children }: { children: ReactNode }) {
   return (
     <Stack
       className={styles.passwordPageContainer}
       sx={{
-        backgroundColor: "blue.main",
+        backgroundColor: "background.secondary",
         paddingTop: { mobile: "124px", tablet: "136px", desktop: "72px" },
       }}
     >
       <Stack className={styles.bgContainer} sx={{ position: "absolute" }}>
-        <Box className={styles.bgGreenCircle} bgcolor="primary.light" />
-        <Box className={styles.bgPurpleCircle} bgcolor="secondary.main" />
-        <Box className={styles.bgCircleOutline} borderColor="primary.main" />
+        <Circle absolute size={400} fillColor="text.brandSecondary" right={0} />
+        <Circle absolute size={200} fillColor="primary.main" bottom={0} right="80px" />
+        <Circle
+          absolute
+          size={248}
+          fillColor="transparent"
+          borderColor="secondary.main"
+          bottom="8px"
+        />
       </Stack>
       <Stack className={styles.passwordPageContents}>{children}</Stack>
       <Typography variant="label3">{new Date().getFullYear()} © Kek Labs.</Typography>
