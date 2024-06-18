d5104b8b83d06f208080ac7a4b35980a420c0eb5
Phenelo <iamdionisiodenmark@gmail.com>
2024-06-18 16:23:00 +0800

    removes elements folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({ children } : Readonly<{
           <ThemeProvider theme={theme}>
             <Provider>
               <CssBaseline />
-              <ClientProviders user={userSession}>
+              <ClientProviders user={userSession?.data || null}>
                 <Navbar />
                 <Container>
                   {children}
